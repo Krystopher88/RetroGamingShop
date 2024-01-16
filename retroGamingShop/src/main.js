58783bb25e0c727d9e5bc5c8131c404fd178ceb2
Krystopher88 <bichon.christ@gmail.com>
2024-01-16 15:43:33 +0100

    TopNavbar

--- retroGamingShop/src/main.js
+++ retroGamingShop/src/main.js
@@ -1,12 +1,20 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+
 import "./assets/main.css";
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons'
 
-
+library.add(faUserSecret)
+library.add(faCartShopping)
 
 const app = createApp(App);
 
 app.use(router);
 
-app.mount("#app");
+createApp(App)
+.component('font-awesome-icon', FontAwesomeIcon)
+.mount('#app')
